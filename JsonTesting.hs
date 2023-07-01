a13557fbae456cb7ea8fba2956cc7743c64153a4
Antricks <33262801+Antricks@users.noreply.github.com>
2023-07-01 19:57:05 +0200

    add some tests

--- JsonTesting.hs
+++ JsonTesting.hs
@@ -9,3 +9,12 @@ checkEmptyList = runParser jsonObjParser "[]" === Just ("", JsonList [])
 
 checkIntList :: [Int] -> Property
 checkIntList as = runParser jsonObjParser (show as) === Just ("", JsonList (map JsonInt as))
+
+checkFloatList :: [Float] -> Property
+checkFloatList as = runParser jsonObjParser (show as) === Just ("", JsonList (map JsonFloat as))
+
+checkStringList :: [String] -> Property
+checkStringList as = runParser jsonObjParser (show as) === Just ("", JsonList (map JsonString as))
+
+checkNestedIntList :: [[Int]] -> Property
+checkNestedIntList as = runParser jsonObjParser (show as) === Just ("", JsonList (map (JsonList . map JsonInt) as))
